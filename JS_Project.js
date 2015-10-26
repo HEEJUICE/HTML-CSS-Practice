8b2b99eb1e69c617922547fddd4c02b5ac50d59a
kljuyt <HEEJU@gimhuijus-MacBook-Pro.local>
2015-10-26 14:17:19 +0900

    .

---
+++ JS_Project.js
@@ -0,0 +1,3 @@
+for (var i = 0; i < 42; i++) {
+    document.write('<div id=\"box' + i + '\" class=\"box\">' + '</div>')
+}
--- purloined_letter.js
+++ purloined_letter.js
@@ -20,3 +20,15 @@ $(function () {
     });
 
 });
+
+$(document).ready(function () {
+    $('.right').after('<p class="clear">&nbsp</p>');
+});
+
+$(window).scroll(function () {
+    var scrolltop = $(window).scrollTop();
+    if (scrolltop == $(document).height() - $(window).height()) {
+        $('#content').addClass('opened');
+        $('#fill').addClass('open');
+    }
+});
